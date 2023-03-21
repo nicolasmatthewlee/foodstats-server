daa0c94034fd95ce9e7240abdacdbcf1ce1ef75f
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-03-21 15:57:35 -0700

    added route to fetch food data by fdcId

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,31 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-app.get("/", (req, res, next) => {
-    return res.send("/");
-});
+app.get("/api/foods/:id", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield fetch(`https://api.nal.usda.gov/fdc/v1/food/${req.params.id}?api_key=${process.env.KEY}`);
+        if (response.status === 404)
+            return res.json({ error: "not found" });
+        const json = yield response.json();
+        return res.json(json);
+    }
+    catch (e) {
+        return res.json({ error: "an unknown error occurred" });
+    }
+}));
 app.listen(443, () => console.log("listening at port 443..."));
--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res, next) => {
-  return res.send("/");
+app.get("/api/foods/:id", async (req, res, next) => {
+  try {
+    const response = await fetch(
+      `https://api.nal.usda.gov/fdc/v1/food/${req.params.id}?api_key=${process.env.KEY}`
+    );
+
+    if (response.status === 404) return res.json({ error: "not found" });
+    const json = await response.json();
+
+    return res.json(json);
+  } catch (e) {
+    return res.json({ error: "an unknown error occurred" });
+  }
 });
 
 app.listen(443, () => console.log("listening at port 443..."));
